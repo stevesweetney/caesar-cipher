7aafce1d94a5690f2500e1306949332601e020f9
stevesweetney <stevesweetney@gmail.com>
2018-01-03 19:06:06 -0500

    Add Line struct to store content and line number. Add func to apply cipher with key to a Line.

--- main.go
+++ main.go
@@ -8,6 +8,53 @@ import (
 	"github.com/urfave/cli"
 )
 
+// Line is used to store the content of a line in a file
+// and its number
+type Line struct {
+	content string
+	number  int
+}
+
+// check if a rune is a letter in the range (a...z) or (A...Z)
+func isLetter(r rune) bool {
+	if (r < 'a' || r > 'z') && (r < 'A' || r > 'Z') {
+		return false
+	}
+	return true
+}
+
+func applyCipher(line *Line, c chan *Line, key int) {
+	translated := make([]rune, 0, len(line.content))
+
+	for _, r := range line.content {
+		if isLetter(r) {
+			shifted := int(r) + key
+			if r >= 'a' && r <= 'z' {
+				if shifted > 'z' {
+					shifted -= 26
+				} else if shifted < 'a' {
+					shifted += 26
+				}
+			} else {
+				if shifted > 'Z' {
+					shifted -= 26
+				} else if shifted < 'A' {
+					shifted += 26
+				}
+			}
+			translated = append(translated, rune(shifted))
+		} else {
+			translated = append(translated, r)
+		}
+	}
+
+	line.content = string(translated)
+	c <- line
+
+}
+
+// readFile opens a file at the specified path and returns
+// a slice of strings that represents the lines of its content
 func readFile(path string) ([]string, error) {
 	file, err := os.Open(path)
 	if err != nil {
